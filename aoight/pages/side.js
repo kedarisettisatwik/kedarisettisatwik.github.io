648c072579a88364643ec054f119a7d0c17e2644
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2021-12-21 13:21:51 +0530

    s

---
+++ aoight/pages/side.js
@@ -0,0 +1,2 @@
+var pro_dp = document.getElementById("pro_dp");
+pro_dp.style.backgroundImage = "url(https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHJhbmRvbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60)";
