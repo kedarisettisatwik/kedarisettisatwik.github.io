239fe00658c6ac877e575c95bf2512bd4833a483
kedarisetti satwik <74258680+kedarisettisatwik@users.noreply.github.com>
2024-12-14 21:06:01 +0530

    Update script.js

--- gym/src/script.js
+++ gym/src/script.js
@@ -1,1 +1,23 @@
+
+function login(){
+    var loginForm = document.getElementById("loginForm");
+    console.log("log in");
+}
+
+var planType = 0;
+
+function planChange(){
+    var plan = document.getElementById("planSelect");
+    planType = plan.value;
+    changeAmount();
+}
+
+function changeAmount(){
+    var amounts = [["1500","1000","2000","4000"],["4300","2800","5600","11,500"],["8700","5500","11,500","22,000"],["17,000","11,000","22,000","45,000"]];
+    var amountEles = document.querySelectorAll(".amount");
+    for(var i = 0;i < amountEles.length;i++){
+        amountEles[i].innerHTML = amounts[planType][i];
+    }
+}
+
 console.log("Rebuild Fitness");
